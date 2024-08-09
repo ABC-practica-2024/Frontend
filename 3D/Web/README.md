c10be47e77ba10498769b9df526884a24b2c7bd6
ATPEngie <paultresco@gmail.com>
2024-08-09 09:12:54 +0300

    style: made comments and README more clear; cleaned up index.html file.

--- 3D/Web/README.md
+++ 3D/Web/README.md
@@ -1,7 +1,8 @@
 # 3D Web preview
-# Made with THREE JS, in VSCode and Blender.
-# Controls: left click to rotate, right click to pan, scroll to zoom, ctrl-click to select a finding and hide all other objects, ctrl-click again to return to main view.
-# Installation: download in a folder, then run in terminal "npm install" and "npm run dev" in said folder; ctrl-click on the link to open the website.
-# Findings.txt contains position, scale, rotation (all in Vector3 format) and color(in hexadecimal format) in 4 lines per each object. 
-# SiteSection.txt contains the scale of the section in Vector3 format on a line, and on the next line the number of layers.
-# X to hide layers, Z to show them (this does not work in focus view). Layers and focus mode do not interfere with each other.
+## Made with THREE JS, in VSCode and Blender.
+* Camera Controls: left click to rotate, right click to pan, scroll to zoom, ctrl-click to select a finding and hide all other objects, ctrl-click again to return to main view.
+* Other controls: x to hide the top visible layer, z to revert.
+* Installation: download in a folder, then run in terminal "npm install" and "npm run dev" in said folder; ctrl-click on the link to open the website.
+* Findings.txt contains position, scale, rotation (all in Vector3 format) and color(in hexadecimal format) in 4 lines per each object. 
+* SiteSection.txt contains the scale of the section in Vector3 format on a line, and on the next line the number of layers.
+* X to hide layers, Z to show them (this does not work in focus view). Layers and focus mode do not interfere with each other.
--- 3D/Web/main.js
+++ 3D/Web/main.js
@@ -4,19 +4,22 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 //import object loader - this is for importing 3D models
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+//set up the scene
 const scene = new THREE.Scene();
 
+//set up the camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-});
-
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+//we can tweak this position
 camera.position.setZ(15);
 camera.layers.enable(1)
 camera.layers.enable(2)
+
+//set up the renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 function render()
 {
@@ -26,13 +29,15 @@ function render()
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
 ambientLight.layers.enableAll()
-ambientLight.position.y=10;
+ambientLight.position.y=10;//we put the light a bit higher up, otherwise if we put an object at the origin it would block it
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
+  //this is sort of a main update loop
   requestAnimationFrame(animate);
 
+  //we also need to update the controls for them to work
   controls.update();
 
   render();
@@ -40,7 +45,8 @@ function animate() {
 
 animate();
 
-//we need an array of objects holding all findings for the raycasting to work (saves performance)
+//we need an array of objects holding all findings for the raycasting to work, otherwise we would be able to select archeological
+//layers or the site section object itself
 let objects=new Array()
 
 fetch("Findings.txt").then(response=>response.text())
@@ -77,6 +83,7 @@ fetch("Findings.txt").then(response=>response.text())
       }
     });
 
+//o represents the selected object, it is null when we are in the main view
 let o=null
 document.body.onclick = function (event) {
   if (event.ctrlKey) {
@@ -93,9 +100,9 @@ document.body.onclick = function (event) {
       if(intersects.length>0)
       {
         o=intersects[0].object
-        //we clicked on something, so we must hide everything else
+        //we clicked on a finding, so we must hide everything else
         camera.layers.disable(0)
-        camera.layers.disable(2)
+        camera.layers.disable(2)//we hide the archeological layers as well
         o=intersects[0].object
         o.layers.enable(1)
       }
@@ -105,29 +112,28 @@ document.body.onclick = function (event) {
       if(!(o===null))
       {
         //we were in "focused" view and must switch to main view, we must reveal all hidden objects
-        o.layers.disable(1)
-        o=null
-        camera.layers.enable(0)
-        camera.layers.enable(2)
+        o.layers.disable(1)//we need to disable layer 1 for the viewed object, otherwise it would stay visible next time we enter focus view
+        o=null //we set o to null to let the other functions know we are now in the main view
+        camera.layers.enable(0)//make the camera see other objects in the site
+        camera.layers.enable(2)//this way we can see the archeological layers which would be visible to us in main view
       }
     }
   }
 }
 
-//handle layers
+//handle layers and the site section object
 let layers=new Array()
 let currentlayer=0//int - represents the top visible layer
 
 fetch("SiteSection.txt").then(response=>response.text())
   .then(text=>{
-    //do stuff with the text
     let rows=text.split("\r\n");
     let bits
     
     //make the site section object
     bits=rows[0].split(" ")
     let siteScale=new THREE.Vector3(parseFloat(bits[0]),parseFloat(bits[1]),parseFloat(bits[2]))
-    //the scale will be usfeul to us later on for the layers
+    //the scale will be useful to us later on for the layers
     const loader=new GLTFLoader();
     loader.load( 'SiteSection.glb', function ( gltf ) {
     gltf.scene.scale.set(siteScale.x,siteScale.y,siteScale.z)
@@ -139,7 +145,7 @@ fetch("SiteSection.txt").then(response=>response.text())
     //we now need to add the archeological layers
     function addLayer(position,scale)
     {
-      //generate a layer (a cube) with given position, rotation, scale and color
+      //generate a layer (a cube) with given position, rotation, scale and color (WIP)
       const box=new THREE.BoxGeometry(scale.x*2,scale.y*2,scale.z*2);
       const boxMaterial=new THREE.MeshStandardMaterial({color:0xffffff});
       const layer=new THREE.Mesh(box,boxMaterial);
@@ -166,12 +172,14 @@ fetch("SiteSection.txt").then(response=>response.text())
     }
   });
 
-// add listener for keyboard
+// add listener for key press events
 document.body.addEventListener('keydown', keyPressed, false);
 
 function keyPressed(event){
   if(o===null)
   {
+    //we are currently in the main view
+    //we only want to interact with layers in the main view, not in focus view
     switch(event.key) {
       case 'z':
         //make layers visible one by one moving upwards
@@ -185,11 +193,13 @@ function keyPressed(event){
         //make layers invisible one by one moving down
         if(currentlayer<layers.length)
         {
+          //layers only have layer 2, so by disabling this layer we hide them from the camera
           layers[currentlayer].layers.disable(2)
           currentlayer++
         }
         break
     }
-    renderer.render(scene,camera);
+    //we need to rerender everytime we make a change
+    render();
   }
 }
